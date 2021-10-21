c8c88c20a86c3a9b84cda5064b1c3213fc90a422
ilkecan <ilkecan@protonmail.com>
2021-10-22 02:34:00 +0300

    Add enabiz-password-maxlength-restriction-remover

---
+++ enabiz-password-maxlength-restriction-remover/index.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        e-Nabız password maxlength restriction remover
+// @namespace   ilkecan
+// @match       https://enabiz.gov.tr/
+// @grant       none
+// @version     1.0.0
+// @author      ilkecan
+// @description e-Nabız giriş sayfasında şifredeki maksimum uzunluk kısıtlamasını kaldırır.
+// ==/UserScript==
+
+$(document).ready(() => {
+    $("#Sifre").removeAttr("maxlength");
+});
